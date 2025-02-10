c0a437934f2d3ccdfc9edfa100b715704fda44a1
Vedant Panchal <VEDANTPANCHAL.ict22@adaniuni.ac.in>
2025-02-10 22:48:07 +0530

    refactor authentication routes: improve cookie handling and response structure

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -2,6 +2,8 @@
 from fastapi import APIRouter, Cookie, HTTPException,responses,Response, Header, status
 from uuid import uuid4
 from datetime import datetime, timezone
+
+from pydantic import EmailStr
 from app.core.config import EXPIRES_IN
 from fastapi.encoders import jsonable_encoder
 from app.core.connection import db
@@ -22,7 +24,7 @@ PASSWORD_REGEX = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?
 
 router = APIRouter()
 @router.post("/sign-up")
-async def sign_up(data: SignUpType, response: Response):
+async def sign_up(response: Response,data: SignUpType):
     try:
         existing_user = db.table("users").select("*").eq("email", data.email).execute()
         
@@ -32,13 +34,12 @@ async def sign_up(data: SignUpType, response: Response):
                 detail="User already exists"
             )
         
-        otp = str(uuid4().int)[:6]
+        otp:str = str(uuid4().int)[:6]
         send_otp_email(data.email, otp)
-        used = False
-        otp_token = generate_otp_jwt(data.email, otp)
+        otp_token:str = generate_otp_jwt(data.email, otp)
         
         response.set_cookie(
-            key="sign_up_token",
+            key="signup_token",
             value=otp_token,
             max_age=600,
             httponly=True,
@@ -48,10 +49,10 @@ async def sign_up(data: SignUpType, response: Response):
         print(response.headers)
 
         
-        return responses.JSONResponse(
-            content={"message": "OTP Sent Successfully"},
-            status_code=status.HTTP_200_OK
-        )
+        return {
+            "message": "OTP Sent Successfully",
+            "status_code":status.HTTP_200_OK
+        }
         
     except HTTPException:
         raise
@@ -65,21 +66,21 @@ async def sign_up(data: SignUpType, response: Response):
 async def verify_otp(
     response: Response,
     data: create_user,
-    sign_up_token: str | None = Cookie(None)
+    signup_token: str | None = Cookie(None)
 ):
-    if not sign_up_token:
+    if not signup_token:
         return HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, 
             detail="OTP token missing in cookies"
         )
     
     try:
-        print(sign_up_token)
+        print(signup_token)
         otpData = verify_otp_type(
-            token = sign_up_token,
+            token = signup_token,
             otp = data.otp
         )
-        email = verify_token(otpData)
+        email: EmailStr = verify_token(otpData)
         hash_password = hashPass(data.password)
         
         user = UserInDB(
@@ -95,13 +96,13 @@ async def verify_otp(
         user_token = create_token(user, EXPIRES_IN)
         db.table("users").insert(jsonable_encoder(user)).execute()
 
-        response.delete_cookie("sign_up_token")
+        response.delete_cookie("signup_token")
         response.set_cookie(
             key="user_token",
             value=user_token,
             max_age=691200,
             httponly=True,
-            secure=True,
+            secure=False,
             samesite="Lax"
         )
         return {
@@ -126,19 +127,29 @@ async def verify_otp(
         # # )
 
 @router.post("/sign-in")
-async def signIn(data: VerifyUser):
-    response = check_user_present(data.email)
-    if not response.data:
-        return HTTPException(status_code=400, detail="No email exists with this user")
+async def signIn(response: Response,data: VerifyUser):
+    user = check_user_present(data.email)
+    if not user.data:
+        raise HTTPException(status_code=400, detail="No email exists with this user")
 
-    existing_user = UserInDB(**response.data[0])
+    existing_user = UserInDB(**user.data[0])
     
     if not existing_user.is_verified:
-        return HTTPException(status_code=401, detail="User is not verified. Please complete OTP verification")
+        raise HTTPException(status_code=401, detail="User is not verified. Please complete OTP verification")
     
     is_verified = verify(data.password,existing_user.password)
+    
     if is_verified:
         token = create_token(existing_user, EXPIRES_IN)
+        response.set_cookie(
+        key="user_token",
+        value=token,
+        max_age=600,
+        httponly=True,
+        secure=False,
+        samesite="Lax"
+        )
+        print(response.headers)
         return {    
             "message": "User signed in successfully",
             "email": data.email,
@@ -148,7 +159,7 @@ async def signIn(data: VerifyUser):
         return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect Password")
 
 @router.post("/reset-password",response_model=None)
-async def reset_password(response : Response,email : str):
+async def reset_password(response : Response,email : EmailStr):
     user_data = check_user_present(email)
     if not user_data:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="No User Exists With This Email")
@@ -176,19 +187,19 @@ async def reset_password(response : Response,email : str):
 async def reset_password(
     response:Response,
     data: ResetPasswordRequest,
-    reset_cookie: str | None = Cookie(None)
+    reset_password_token: str | None = Cookie(None)
 ):
 
     entered_otp = data.entered_otp
     newPassword = data.new_password
-    if not reset_cookie:
+    if not reset_password_token:
         return HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, 
             detail="OTP token missing in cookies"
         )
     
     token_data = verify_otp_type(
-        token=reset_cookie,
+        token=reset_password_token,
         otp=entered_otp
     )
     email = verify_token(token_data)
@@ -203,11 +214,16 @@ async def reset_password(
         hashedPassword = hashPass(newPassword)
         try:
             db.table("users").update({"password": hashedPassword}).eq("email", email).execute()
-            response.delete_cookie("reset_cookie")
+            response.delete_cookie("reset_password_token")
             return{
                 "message" : "Password Updated Successfully"
             }
         except:
             raise HTTPException(status_code=status.HTTP_408_REQUEST_TIMEOUT,detail="Error Ocurred While Updating Password")
 
-
+@router.post("/logout")
+async def logout(response: Response):
+    response.delete_cookie("user_token")
+    return {
+        "message": "User logged out successfully"
+    }
